02574bcf90dd8094a3179698b4efa9704185e37b
Jan Husak <jan.husak@ysoft.com>
2022-09-04 17:16:42 +0200

    users

--- sql/V1__Initial.sql
+++ sql/V1__Initial.sql
@@ -1,3 +1,6 @@
-CREATE TABLE MyTable (
-    MyColumn VARCHAR(100) NOT NULL
+CREATE TABLE Users (
+    Id int NOT NULL AUTO_INCREMENT,
+	Sub varchar(255) NOT NULL,
+	Name varchar(255) NOT NULL,
+	PRIMARY KEY (Id)
 );
