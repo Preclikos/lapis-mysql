cde135a531670e371811ef8884ed261f397e2548
Jan Husak <jan.husak@ysoft.com>
2022-09-09 14:53:38 +0200

    lapises and codes

---
+++ sql/V1__LapisCodes.sql
@@ -0,0 +1,8 @@
+CREATE TABLE LapisCodes (
+    LapisId int NOT NULL,
+	Country int NOT NULL,
+	Region int NOT NULL,
+	User int NOT NULL,
+	Lapis int NOT NULL,
+	PRIMARY KEY (Id)
+);
---
+++ sql/V1__Lapises.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Lapises (
+    Id int NOT NULL AUTO_INCREMENT,
+	Name varchar(255) NOT NULL,
+	Description varchar(1000) NOT NULL,
+	PRIMARY KEY (Id)
+);
