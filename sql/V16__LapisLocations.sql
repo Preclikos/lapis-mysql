a8538b3b47e89fb280ec888078750df7e4e5d072
Jan Husak <jan.husak@ysoft.com>
2022-09-20 16:36:00 +0200

    fix decimal

--- sql/V16__LapisLocations.sql
+++ sql/V16__LapisLocations.sql
@@ -1,8 +1,8 @@
 CREATE TABLE LapisLocations (
     Id int NOT NULL AUTO_INCREMENT,	
-	Lat DECIMAL(3,6) NOT NULL,
-	Lng DECIMAL(3,6) NOT NULL,
-	Alt DECIMAL(3,6) NOT NULL,
+	Lat DECIMAL(6,6) NOT NULL,
+	Lng DECIMAL(6,6) NOT NULL,
+	Alt DECIMAL(6,6) NOT NULL,
 	Country varchar(100) NULL,
 	City varchar(100) NULL,
 	LapisId int NOT NULL,
